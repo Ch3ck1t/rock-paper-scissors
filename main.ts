7b7e1412e8922afcec7a622ee99665bd20e3af9e
Ch3ck1t <66013100+Ch3ck1t@users.noreply.github.com>
2022-02-22 15:32:07 -0500

    Fichiers initiaux du projet MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+let main = 0
+input.onGesture(Gesture.Shake, function () {
+    main = randint(1, 3)
+    if (main == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (main == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
